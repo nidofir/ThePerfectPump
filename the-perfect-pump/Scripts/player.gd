cfcd40e253b6f138f58dc2e71ed6a24f5c5c9e45
nidofir <124328491+nidofir@users.noreply.github.com>
2026-04-14 12:55:19 +1000

    added 3d levels, created player scene, organisational housekeeping
    renamed "MainGame" to "FeatureTest2D", added a 3D level to populate with assets, and a secondary level for prototyping and testing. created player scene and added code for camera to follow mouse movement

---
+++ the-perfect-pump/Scripts/player.gd
@@ -0,0 +1,50 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+const SENSITIVITY = 0.03
+
+var gravity = 9.8
+
+@onready var head: Node3D = $Head
+@onready var player_camera: Camera3D = $Head/PlayerCamera
+
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * SENSITIVITY)
+		player_camera.rotate_x(-event.relative.y * SENSITIVITY)
+		player_camera.rotation.x = clamp(player_camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+		
+		
+		
+
+
+
+
+func _physics_process(delta: float):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = 0.0
+		velocity.z = 0.0
+		
+
+	move_and_slide()
