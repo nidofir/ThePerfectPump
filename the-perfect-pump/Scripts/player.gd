202e054a85225683369473667bcc61057d960120
nidofir <124328491+nidofir@users.noreply.github.com>
2026-04-14 13:14:56 +1000

    player movement
    bound player movement to "wasd" and "left" "right" "up" "down" keys. bound direction of movement to camera rotation so player moves in direction they are facing

--- the-perfect-pump/Scripts/player.gd
+++ the-perfect-pump/Scripts/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-const SENSITIVITY = 0.03
+const SENSITIVITY = 0.02
 
 var gravity = 9.8
 
@@ -37,8 +37,8 @@ func _physics_process(delta: float):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir := Input.get_vector("left", "right", "forward", "back")
+	var direction := (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
