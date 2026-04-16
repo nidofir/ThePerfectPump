ba8d4eb098358f0f47429b7b24936566d4be9ccd
nidofir <124328491+nidofir@users.noreply.github.com>
2026-04-16 20:20:21 +1000

    sprite randomization beginnings

--- the-perfect-pump/Scripts/npc_popup.gd
+++ the-perfect-pump/Scripts/npc_popup.gd
@@ -1,6 +1,29 @@
 extends Node3D
 
 @onready var car: Node3D = $Car
-@onready var sprite_3d: Sprite3D = $Sprite3D
 
-AnimationPlayer()
+#
+#@onready var npc_sprite: Sprite3D = $NPC_sprite
+ #
+#var new_texture = [
+	#load("res://Assets/Sprites/Character_1.png"),
+	#load("res://Assets/Sprites/Character_2.png"),
+	#load("res://Assets/Sprites/Character_3.png"),
+	#load("res://Assets/Sprites/Character_4.png"),
+	#load("res://Assets/Sprites/Character_5.png"),
+	#load("res://Assets/Sprites/Character_6.png"),
+	#load("res://Assets/Sprites/Character_7.png"),
+	#load("res://Assets/Sprites/Character_8.png"),
+	#load("res://Assets/Sprites/Character_9.png"),
+	#load("res://Assets/Sprites/Character_10.png"),
+	#load("res://Assets/Sprites/Character_11.png"),
+	#load("res://Assets/Sprites/Character_12.png"),
+	#load("res://Assets/Sprites/Character_13.png"),
+	#load("res://Assets/Sprites/Character_14.png"),
+	#load("res://Assets/Sprites/Character_15.png")
+	#]
+#
+#
+#
+#func _on_Arrow_pressed() -> void:
+	#my_sprite.texture = new_texture[0]
