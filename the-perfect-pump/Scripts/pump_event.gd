5fb149a041651ed93d3e47fa16428237d2c157f7
nidofir <124328491+nidofir@users.noreply.github.com>
2026-04-16 17:14:05 +1000

    added the quick time event as a canvas layer over the top of the 3d scene
    can now "play" the pump minigame while inside the 3d scene/pump. now there is bug where mouse input is not captured while in 3d scene. minigame still not entirely functional(indicator does not move lol)

---
+++ the-perfect-pump/Scripts/pump_event.gd
@@ -0,0 +1,157 @@
+extends CanvasLayer
+
+
+
+enum State {IDLE, WAITING,ENGAGE, PUMP, SUCCESS, FAIL}
+
+var current_state: State= State.IDLE 
+
+var point_count: int = 0
+var indicator_speed: float = 300.0
+var indicator_direction: int= 1
+var current_NPC: String = ""
+var current_NPC_index:int=0
+var current_NPC_difficulty:float=0.0
+
+@export var NPC_names: Array[String]=[
+	"easiest","easy","mediocre","hard","harder","hardest"
+]
+@export var NPC_difficulty:Array[int]=[60,20,10,7,2,1]
+@export var NPC_difficulty_zone_widths: Array[int]=[130,100,80,65,50,35]
+
+@onready var new_npc_timer: Timer = $NewNPCTimer
+@onready var pump_timer: Timer = $PumpTimer
+@onready var fuel_bar: Control = $FuelBar
+@onready var indicator: ColorRect = $FuelBar/Indicator
+@onready var points_label: Label = $UI/PointsLabel
+@onready var status_label: Label = $UI/StatusLabel
+@onready var target_zone: ColorRect = $FuelBar/TargetZone
+
+
+
+#@onready var points_label: Label = $PumpEventGameLayer/UI/PointsLabel
+#@onready var status_label: Label = $PumpEventGameLayer/UI/StatusLabel
+#
+#@onready var fuel_bar: Control = $PumpEventGameLayer/FuelBar
+#@onready var indicator: ColorRect = $PumpEventGameLayer/FuelBar/Indicator
+#@onready var target_zone: ColorRect = $PumpEventGameLayer/FuelBar/TargetZone
+#
+#@onready var new_npc_timer: Timer = $PumpEventGameLayer/NewNPCTimer
+#@onready var pump_timer: Timer = $PumpEventGameLayer/PumpTimer
+
+func _ready()-> void:
+	fuel_bar.visible=false
+	new_npc_timer.timeout.connect(_on_new_npc_timer_timeout)
+	pump_timer.timeout.connect(_on_pump_timer_timeout)
+	_set_state(State.IDLE)
+
+
+func process(delta:float)->void:
+	if current_state != State.PUMP:
+		return
+	_move_indicator(delta)
+
+
+
+func _move_indicator(delta:float)->void:
+	var max_x: float= fuel_bar.size.x-indicator.size.x
+	indicator.position.x+= indicator_speed* indicator_direction*delta
+	if indicator.position.x >= max_x:
+		indicator.position.x=max_x
+		indicator_direction = -1
+	elif indicator.position.x<=0.0:
+		indicator.position.x=0.0
+		indicator.direction=1
+
+func _reset_indicator()->void:
+	var max_x: float = fuel_bar.size.x - indicator.size.x
+	indicator.position.x=randf_range(0.0,max_x)
+	indicator_direction =1 if randf()>0.5 else -1
+
+func _set_state(new_state:State)->void:
+	current_state = new_state
+	match new_state:
+		State.IDLE:
+			status_label.text="press SPACE to summon customers"
+			fuel_bar.visible= false
+		State.WAITING:
+			status_label.text="waiting for customer"
+		State.ENGAGE:
+			status_label.text="THERES A CUSTOMER, PRESS AND HOLD SPACE TO START THE PUMP"
+		State.PUMP:
+			status_label.text="STOP THE BAR IN THE GREEN ZONE"
+			fuel_bar.visible=true
+			target_zone.size.x= NPC_difficulty_zone_widths[current_NPC_index]
+			var zone_max_x:float = fuel_bar.size.x - target_zone.size.x
+			target_zone.position.x=randf_range(0.0, zone_max_x)
+			#_reset_indicator()
+			pump_timer.wait_time= randf_range(3.0,6.0)
+			pump_timer.start()
+		State.SUCCESS:
+			status_label.text="PERFECT PUMP"
+			fuel_bar.visible=false
+		State.FAIL:
+			status_label.text="BAD PUMP"
+			fuel_bar.visible=false
+
+
+
+func _input(event:InputEvent)->void:
+	if event.is_action_pressed("ui_accept"):
+		match current_state:
+			State.IDLE:
+				_summon_customer()
+			State.ENGAGE:
+				_start_pump()
+			State.PUMP:
+				_stop_pump()
+			
+
+func _summon_customer()->void:
+	_set_state(State.ENGAGE)
+	new_npc_timer.wait_time=randf_range(1.5,3.5)
+	new_npc_timer.start()
+	_set_state(State.WAITING)
+	
+func _start_pump()->void:
+	new_npc_timer.stop()
+	#_pick_NPC()
+	_set_state(State.ENGAGE)
+
+func _on_new_npc_timer_timeout()->void:
+	_set_state(State.PUMP)
+	pump_timer.wait_time=randf_range(1.5,2.5)
+	pump_timer.start()
+
+func _on_pump_timer_timeout()->void:
+	_fail()
+	
+
+func _stop_pump()->void:
+	pump_timer.stop()
+	var ind_left:float=indicator.position.x
+	var ind_right:float=ind_left +indicator.size.x
+	var zone_left:float=target_zone.position.x
+	var zone_right: float= zone_left +target_zone.size.x
+	
+	if ind_left>=zone_left and ind_right<=zone_right:
+		_success()
+	else:
+		_fail()
+
+func _success()->void:
+	point_count+=1
+	var npc:String=current_NPC
+	points_label.text="Points %d" %point_count
+	status_label.text="blood served"
+	indicator_speed=min(indicator_speed+20.0,600.0)
+	_set_state(State.SUCCESS)
+	status_label.text="you served %s"%[NPC_names]
+	await get_tree().create_timer(1.5).timeout
+	_set_state(State.IDLE)
+
+func _fail()->void:
+	_set_state(State.FAIL)
+	status_label.text="blood spilled"
+ 	#indicator_speed=min(indicator_speed)
+	_set_state(State.IDLE)
